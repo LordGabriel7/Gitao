2151e624ca9b74e8bf7a724539d30cebd217f1fa
Lord Gabriel <gp04501@gmail.com>
2024-11-25 14:01:05 -0300

    Um outro aí

--- src/com/Gitao/projeto/Principal/Main.java
+++ src/com/Gitao/projeto/Principal/Main.java
@@ -3,5 +3,6 @@ package com.Gitao.projeto.Principal;
 public class Main {
     public static void main (String[] args) {
         System.out.printf("Olá, mundo");
+        System.out.printf("CapaCapa");
     }
 }
